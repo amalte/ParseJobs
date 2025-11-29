0e8f4452dd6fec6ac94777ca1e4fdf200c12439f
Elias Kihl <elias.kihltrom@gmail.com>
2025-11-29 19:24:49 +0100

    main.py -> jsonparser.py

---
+++ jsonparser.py
@@ -0,0 +1,29 @@
+import json
+
+import localLLM
+
+keywords = ["junior", "nybörjare"]
+
+
+def parse_jsonfile(fileName):
+    with open(fileName, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
+def clean_json(data):
+    cleaned_data = []
+    for entry in data:
+        description = entry["jobDescription"]
+        if localLLM.isJuniorJob(description):
+            entry["viewJobLink"] = "https://se.indeed.com" + entry["viewJobLink"]
+            cleaned_data.append(entry)
+
+    # Print all job links
+    for job in cleaned_data:
+        print(job["viewJobLink"])
+
+
+if __name__ == '__main__':
+    clean_json(parse_jsonfile("extractedJobs.json"))
